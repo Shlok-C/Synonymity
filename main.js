a59462bd54594c7663b6c103f0dceee628603bf5
Shlok Chatterjee <shlok.chatterjee007@gmail.com>
2023-10-28 12:13:46 -0500

    First commmit: create initial files

---
+++ main.js
@@ -0,0 +1,8 @@
+const word = document.getElementById("word")
+const findButton = document.getElementById("findButton")
+
+const synResult = document.getElementById("synResult")
+const defResult = document.getElementById("defResult")
+
+const API_KEY = ""
+const apiURL = ""
